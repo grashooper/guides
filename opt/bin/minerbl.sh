5ba9e39e610435618247ab7a6db0871ed72daf97
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-06-18 13:28:50 +0000

    minerbl.sh

--- opt/bin/minerbl.sh
+++ opt/bin/minerbl.sh
@@ -5,5 +5,5 @@ wget -O /opt/etc/miner.conf https://zerodot1.gitlab.io/CoinBlockerLists/list.txt
 
 echo Format Block List
 cd /opt/etc/
-sed -i "/myètherwället.com/d; /myètherwället.com/d" miner.conf
+sed -i "/myètherwället.com/d; /mÿethèrwallét.com/d" miner.conf
 sed -i 's/.*/ipset=\/&\/miner/' miner.conf
