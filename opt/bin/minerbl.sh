30e52b5367d9f17a28f93dc1cbb92cc98d839f0a
blackcofee <42971175+blackcofee@users.noreply.github.com>
2018-06-26 13:35:59 +0000

    minerbl.sh

--- opt/bin/minerbl.sh
+++ opt/bin/minerbl.sh
@@ -3,6 +3,9 @@
 echo Download List
 wget -O /opt/etc/miner.conf https://gitlab.com/ZeroDot1/CoinBlockerLists/raw/master/list_browser.txt
 
-echo Format Block List
+echo Generation Block List
 cd /opt/etc/
 sed -i 's/.*/ipset=\/&\/miner/' miner.conf
+
+echo Restart dnsmasq
+killall -SIGHUP dnsmasq
