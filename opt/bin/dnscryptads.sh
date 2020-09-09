840b1828efb80abaad7e66a832c66a5ecdbd86b5
blackcofee <42971175+blackcofee@users.noreply.github.com>
2020-09-09 10:38:49 +0900

    Update dnscryptads.sh

--- opt/bin/dnscryptads.sh
+++ opt/bin/dnscryptads.sh
@@ -2,7 +2,7 @@
 
 echo Generated List 
 cd /opt/etc/dnscrypt
-python generate-domains-blacklist.py -i > blacklist.txt
+python generate-domains-blocklist.py -i > blacklist.txt
 
 echo Restart dnscrypt
 /opt/etc/init.d/S09dnscrypt-proxy2 restart
